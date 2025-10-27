6026d2b91c7aa223082e76079cdd8f52369abeb8
SikrutiMahapatra <tia10462456@gmail.com>
2025-10-27 19:35:12 +0530

    updated

---
+++ script.js
@@ -0,0 +1,150 @@
+// console.log("hello world")
+//this is single line comment
+/*multi
+line*/
+// variable and constant
+// operators:
+// arithmatic:+,-,*,/,% etc
+// logic:<=,>=,==,!=
+// assign:=,+=,-=,*=,**=,/=etc
+// ternary:num%2==0?"even":"odd"
+
+// if,if else,else if,nested if
+
+// for,while,do while,break,continue
+
+// array
+//    let arr=[1,2,3,4,5]
+   
+// //    console.log(arr[2]+arr[0])
+// //    arr.push="satin"
+// //    arr.pop()
+// //    arr.unshift("neel")
+
+// //    console.log(arr.includes("neel"))
+// //    console.log(arr.indexOf("neel"))
+//    let doubled=arr.map(db => db*2)
+//    console.log(doubled)
+//    let even=arr.filter(num=>num%2==0)
+//    console.log(even)
+//    let sum=arr.reduce((acc,num)=>acc+num,0)
+//    console.log(sum)
+//    let firsteven=arr.find(num=>num%2==0)
+//    console.log(firsteven)
+//    let findind=arr.findIndex(num=>num%2==0)
+//      console.log(findind)
+//     let someeven=arr.some(num=>num%2==0)
+//       console.log(someeven)
+
+//      let everyeven=arr.every(num=>num%2==0)
+//       console.log(everyeven) 
+       //sortin
+    // let num=[14,2,3,453,45353,,655675,65733,22]
+    // let alpha =["s","d","y","a"]
+    // alpha.sort()
+    // console.log(alpha)
+    // num.sort((a,b)=>a-b)
+    // console.log(num)
+    // let joiinn=alpha.join("")
+    //console.log(joiinn)
+
+
+    // objects and methods....
+// let person={
+//     name:"john",
+//     age:20,
+//     city:"bhubaneshwar",
+//     greet:function(){
+//         console.log("this is js")
+//     },
+//     subject:{
+//         maths:65,
+//         science:78
+//     }
+// }
+// person.gender="male"
+// delete person.city
+// console.log("name" in person)
+// console.log(person)
+// console.log(person.age)
+// console.log(person["age"])
+// console.log(person.greet())
+// let keys=Object.keys(person)
+// console.log(keys)
+// let value=Object.values(person)
+// console.log(value)
+// let entry=Object.entries(person)
+// console.log(entry)
+// let newperson = Object.assign({},person);
+// console.log(newperson)
+// Object.freeze(person)  
+// person.num=1;
+// console.log(person)
+
+
+
+
+//set
+// let number=new set([1,2,3,4,5,6,6,7,88,7])
+
+// console.log(number)
+// let neww = new Set()
+// neww.add("apple")
+// neww.add("orange")
+
+// console.log(neww.has("banana"))
+// console.log(neww.size)
+// console.log(neww.delete("apple"))
+// console.log(neww)
+// neww.clear()
+// console.log(neww)
+
+
+// let colors = new Set(["violet", "blue", "orange", "red"]);
+
+// for (let color of colors) {
+//     console.log(color);
+// }
+// colors.forEach(color=>console.log(color))
+// let usermap= new Map();
+// usermap.set("name","siku")
+// usermap.set("place","bbs")
+// usermap.set("roll",24051056)
+// console.log(usermap)
+// console.log(usermap.get("name"))
+// console.log(usermap.has("name"))
+// for(let [key,value] of usermap){
+//     console.log(`${key}:${value}`)
+// }
+
+
+//for in ,for of
+
+
+// let fruits=["apple","mango"]
+// // for( let index in fruits){
+// //     console.log(index)
+// //     console.log(fruits[index])
+// // }
+// for( let index of fruits){
+//     console.log(index)
+    
+// }
+//  let person={     name:"john",
+//     age:20,
+//     city:"bhubaneshwar",
+//     greet:function(){
+//         console.log("this is js")
+//     },
+//     subject:{
+//         maths:65,
+//         science:78
+//     }
+// }
+// for(let key in person){
+//     console.log(key)
+// }
+// for(let key of Object.values(person)){
+//     console.log(key)
+// }
+
